3bfbe22c605f4e0c21dcecd00713499dc19ed2fe
Liehan Els <ST10085345@vcconnect.edu.za>
2024-08-13 10:53:39 +0200

    FIXED IT more

--- app/src/main/java/com/example/app_ui_design/LoginFragment.kt
+++ app/src/main/java/com/example/app_ui_design/LoginFragment.kt
@@ -1,19 +1,53 @@
 package com.example.app_ui_design
 
 import android.os.Bundle
+import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 
 class LoginFragment : Fragment() {
+    private lateinit var btnLogin: Button
+    private lateinit var btnBiometric: Button
+    private lateinit var txtInputPasswordLogin: EditText
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.login_fragment, container, false)
+
+        btnLogin = view.findViewById(R.id.btnLogin)
+        btnBiometric = view.findViewById(R.id.btnBiometric)
+        txtInputPasswordLogin = view.findViewById(R.id.txtInputPasswordLogin)
+
+        btnLogin.setOnClickListener {
+            if (!isPasswordValid(txtInputPasswordLogin.text)) {
+                txtInputPasswordLogin.error = "Error"
+            } else {
+                txtInputPasswordLogin.error = null
+                (activity as NavigationHost).navigateTo(ProductGridFragment(), false)
+            }
+        }
+        btnBiometric.setOnClickListener {
+
+        }
+
+        txtInputPasswordLogin.setOnKeyListener { _, _, _ ->
+            if (isPasswordValid(txtInputPasswordLogin.text)) {
+                txtInputPasswordLogin.error = null
+            }
+            false
+        }
+
         return view
     }
-    //password check goes here
+
+    private fun isPasswordValid(text: Editable?): Boolean {
+        return text != null && text.length >= 8
+    }
 }
